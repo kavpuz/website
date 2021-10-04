55e44e4a46dff1a1d28df2a7b21ae15005cb6baf
eminarican <emin.arican@outlook.com>
2021-10-04 16:53:40 +0300

    bg color

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React, { CSSProperties } from "react";
 export default class Index extends React.Component {
 
   render() {
-    return(<div className="w-screen h-screen flex justify-center items-center">
+    return(<div className="w-screen h-screen flex justify-center items-center bg-gray-50">
       <div className="text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">SOON</div>
     </div>)
   }
