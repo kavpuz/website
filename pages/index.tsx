44dee2704d57763d33ec9027b7e206ae0f49342b
eminarican <emin.arican@outlook.com>
2021-10-04 16:31:44 +0300

    init

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { CSSProperties } from "react";
+
+export default class Index extends React.Component {
+
+  render() {
+    return(<div className="w-screen h-screen flex justify-center items-center">
+      <div className="text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">SOON</div>
+    </div>)
+  }
+}
