709f83eef776b83562e7438ebac4b9bbe04568b4
eminarican <emin.arican@outlook.com>
2021-10-04 17:21:13 +0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import React, { CSSProperties } from "react";
 export default class Index extends React.Component {
 
   render() {
-    return(<div className="w-screen h-screen flex justify-center items-center bg-gray-50">
-      <div className="text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">SOON</div>
+    return(<div className="w-screen h-screen flex justify-center items-center">
+      <img width="300" height="300" src="/logo.png"/>
     </div>)
   }
 }
